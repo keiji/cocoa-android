6331c1b61320011135460f6e65d90f229b7556a3
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-09-18 20:58:02 +0900

    Create project.

---
+++ app/src/main/java/dev/keiji/cocoa/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.keiji.cocoa.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
