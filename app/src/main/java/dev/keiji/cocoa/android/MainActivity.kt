66f88b5d7cbb28ce45499b906fa4c21640250536
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-09-20 14:52:24 +0900

    Refactoring.

--- app/src/main/java/dev/keiji/cocoa/android/MainActivity.kt
+++ app/src/main/java/dev/keiji/cocoa/android/MainActivity.kt
@@ -3,12 +3,10 @@ package dev.keiji.cocoa.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
