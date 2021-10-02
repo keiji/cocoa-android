783c703dee425925b4874ff05815978659cbf865
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-10-02 10:07:37 +0900

    Update libraries version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,22 +46,22 @@ dependencies {
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
 
     implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("androidx.compose.material:material:1.0.2")
-    implementation("androidx.compose.material:material-icons-extended:1.1.0-alpha04")
+    implementation("androidx.compose.material:material:1.0.3")
+    implementation("androidx.compose.material:material-icons-extended:1.1.0-alpha05")
 
-    implementation("androidx.compose.ui:ui-tooling:1.0.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-beta01")
-    implementation("com.google.android.material:compose-theme-adapter:1.0.2")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha09")
+    implementation("androidx.compose.ui:ui-tooling:1.0.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-rc01")
+    implementation("com.google.android.material:compose-theme-adapter:1.0.3")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
 
-    implementation("androidx.compose.compiler:compiler:1.1.0-alpha04")
-    implementation("androidx.compose.runtime:runtime:1.1.0-alpha04")
-    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-alpha04")
-    implementation("androidx.compose.foundation:foundation:1.1.0-alpha04")
-    implementation("androidx.compose.ui:ui:1.1.0-alpha04")
+    implementation("androidx.compose.compiler:compiler:1.1.0-alpha05")
+    implementation("androidx.compose.runtime:runtime:1.1.0-alpha05")
+    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-alpha05")
+    implementation("androidx.compose.foundation:foundation:1.1.0-alpha05")
+    implementation("androidx.compose.ui:ui:1.1.0-alpha05")
 
     api("androidx.navigation:navigation-fragment-ktx:2.3.5")
 
@@ -71,5 +71,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.2")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.3")
 }
