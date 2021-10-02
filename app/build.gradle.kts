4aad4e69f986e7e3525bf8cb7d04471c1a9431a2
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-10-02 09:51:33 +0900

    Add Timber.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
 
     api("androidx.navigation:navigation-fragment-ktx:2.3.5")
 
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
---
+++ app/src/main/java/dev/keiji/cocoa/android/MainApplication.kt
@@ -0,0 +1,15 @@
+package dev.keiji.cocoa.android
+
+import android.app.Application
+import timber.log.Timber
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
