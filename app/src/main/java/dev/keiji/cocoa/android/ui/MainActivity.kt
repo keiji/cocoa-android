bba707df4e27ff331ecba74632be3116709e7ace
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-10-03 19:44:57 +0900

    Fixed an issue that caused the fragment to load twice.

--- app/src/main/java/dev/keiji/cocoa/android/ui/MainActivity.kt
+++ app/src/main/java/dev/keiji/cocoa/android/ui/MainActivity.kt
@@ -2,10 +2,8 @@ package dev.keiji.cocoa.android.ui
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import dev.keiji.cocoa.android.R
 import dev.keiji.cocoa.android.ExposureNotificationWrapper
@@ -14,18 +12,6 @@ import dev.keiji.cocoa.android.ExposureNotificationWrapper
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val viewModel: ExposureNotificationViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        if (savedInstanceState == null) {
-            navController.navigate(R.id.homeFragment)
-        }
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
