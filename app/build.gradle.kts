2354062e2d925a9c6d6a07ccc48bfb1ba5598eb2
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-10-17 10:58:47 +0900

    Update library version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -199,7 +199,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-base:17.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.2")
 
-    implementation("dev.keiji.rfc4648:rfc4648:0.8.0")
+    implementation("dev.keiji.rfc4648:rfc4648:0.9.0")
 
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
